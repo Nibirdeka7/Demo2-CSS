bdd51a11346db6d7d910671b2d53533e1a438bb4
Nibir <nibirdeka70@gmail.com>
2025-09-12 21:57:38 +0530

    Merge Conflict Solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Member from './pages/Member'
-import Home from './pages/Home'  
-import Layout from './components/Layout'
+import Home from './pages/Home'
 import Developers from './pages/Developers'
+import  Wings from './pages/Wings'
+import EditorialsComingSoon from './pages/Editorials'
 const App = () => {
   return (
     <BrowserRouter>
@@ -11,6 +12,8 @@ const App = () => {
         <Route path="/" element={<Home />} />
         <Route path="/members" element={<Member />} />
         <Route path="/developers" element={<Developers />} />
+        <Route path="/wings" element={<Wings />} />
+        <Route path="/editorials" element={<EditorialsComingSoon />} />
       </Routes>
     </BrowserRouter>
   )
